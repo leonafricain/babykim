b76bed62117b51bc77dd4eaa0ed531c275aacb4a
leonafriciain <kimleonafricain@gmail.com>
2021-09-01 13:19:36 +0200

    second  commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
     <Header />
+    hello
     <SideBar />
-    <main class="pt-16 px-7 transition-all duration-300 ease-linear mr-52" :class="collapsed ? 'ml-16': 'ml-60'">
+    <main
+      class="pt-16 px-7 transition-all duration-300 ease-linear mr-52"
+      :class="collapsed ? 'ml-16' : 'ml-60'"
+    >
       <Nuxt />
     </main>
   </div>
 </template>
 
 <script>
-import NavBar from '~/components/NavBar.vue';
+import NavBar from "~/components/NavBar.vue";
 export default {
   components: { NavBar },
-    computed:{
-        collapsed() {
-            return this.$store.state.collapsed
-        }
-    }
+  computed: {
+    collapsed() {
+      return this.$store.state.collapsed;
+    },
+  },
 };
 </script>
 
